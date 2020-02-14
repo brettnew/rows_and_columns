dd8f0ac17cc853220778ff17f16b084453848cfe
Brett New <jumpingneptune@gmail.com>
2020-02-14 12:34:57 -0600

    Make some squares

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,50 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Rows and Columns',
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('Rows and Columns'),
-          ),
-          body: Center(
-            child: Text('Hiya'),
-          )),
+        appBar: AppBar(
+          title: Text('Rows and Columns'),
+        ),
+        backgroundColor: Colors.indigo[100],
+        body: Home(),
+      ),
     );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final sizeX = MediaQuery.of(context).size.width;
+    final sizeY = MediaQuery.of(context).size.height;
+    return Container(
+      width: sizeX,
+      height: sizeY,
+      child: Column(
+        children: createSquares(5),
+      ),
+    );
+  }
+
+  List<Widget> createSquares(int numSquares) {
+    int i = 0;
+    List colors = [
+      Colors.amber,
+      Colors.deepPurple,
+      Colors.deepOrange,
+      Colors.indigo,
+      Colors.lightBlue
+    ];
+    List<Widget> squares = List<Widget>();
+    while (i < numSquares) {
+      Container square = Container(
+        color: colors[i],
+        width: 60,
+        height: 60,
+        child: Text(i.toString()),
+      );
+      i++;
+      squares.add(square);
+    }
+    return squares;
   }
 }
