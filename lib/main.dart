87a127abcc343079aa2a961ed0d4d5e33d954934
Brett New <jumpingneptune@gmail.com>
2020-02-14 18:01:51 -0600

    Have fun with some pizza

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Rows and Columns',
+      title: 'Stack',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Rows and Columns'),
+          title: Text('Stack'),
         ),
         backgroundColor: Colors.indigo[100],
         body: Home(),
@@ -26,12 +26,8 @@ class Home extends StatelessWidget {
     return Container(
       width: sizeX,
       height: sizeY,
-      child: Row(
-        // verticalDirection: VerticalDirection.up,
-        // textDirection: TextDirection.rtl,
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: createSquares(5),
+      child: Stack(
+        children: showPizzaLayout(sizeX, sizeY),
       ),
     );
   }
@@ -46,18 +42,77 @@ class Home extends StatelessWidget {
       Colors.lightBlue
     ];
     List<Widget> squares = List<Widget>();
+    squares.add(Container(color: Colors.black));
     while (i < numSquares) {
-      Expanded square = Expanded(
-          flex: i,
+      Positioned square = Positioned(
+          top: 100 + i.toDouble() * 100,
+          left: 25 + i.toDouble() * 25,
           child: Container(
             color: colors[i],
-            // width: 60,
-            height: 60,
+            width: 60.0 * (numSquares - i),
+            height: 60.0 * (numSquares - i),
             child: Text(i.toString()),
           ));
       i++;
       squares.add(square);
     }
     return squares;
+  }
+
+  List<Widget> showPizzaLayout(double sizeX, double sizeY) {
+    List<Widget> layoutChildren = List<Widget>();
+    Container backGround = Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: NetworkImage('http://bit.ly/pizzaimage'),
+            fit: BoxFit.fitHeight),
+      ),
+    );
+    layoutChildren.add(backGround);
+    Positioned pizzaCard = Positioned(
+      top: sizeY / 20,
+      left: sizeX / 20,
+      child: Card(
+        elevation: 12,
+        color: Colors.white70,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Pizza Margherita',
+              style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.deepOrange[800]),
+            ),
+            Padding(
+              padding: EdgeInsets.all(12),
+              child: Text(
+                'This delicious pizza is made of Tomato, \nMozzarella, and Basil. \n\n Seriously you can\'t miss it',
+                style: TextStyle(fontSize: 18, color: Colors.grey[800]),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+    layoutChildren.add(pizzaCard);
+    Positioned buttonOrder = Positioned(
+      bottom: sizeY / 20,
+      left: sizeX / 20,
+      width: sizeX - sizeX / 10,
+      child: RaisedButton(
+        elevation: 12,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        color: Colors.orange[900],
+        child: Text('Order Now!',
+            style: TextStyle(fontSize: 20, color: Colors.white)),
+        onPressed: () {},
+      ),
+    );
+    layoutChildren.add(buttonOrder);
+    return layoutChildren;
   }
 }
